36f41adfb51fc31461e46f3f2fc24a118d04a73c
AliiHussein <aliunali@yahoo.com>
2023-09-13 06:55:27 +0300

    calculator updates

--- Calculator_app/Calculator_app/main.c
+++ Calculator_app/Calculator_app/main.c
@@ -5,47 +5,94 @@
  * Author : Ali
  */ 
 
+// Libraries
 #include "std_macros.h"
 #include "LCD.h"
 #include "KEYPAD.h"
+#include <math.h>
 
+// functions prototype
 uint8_t get_input_from_user(void);
-int8_t get_calc_value(uint8_t a, uint8_t c, uint8_t b);
+int32_t get_calc_value(uint32_t a, uint8_t c, uint32_t b);
+
+#define max_digits 3
+#define DEBUG1 1
 
 int main(void)
 {
+	// Initializations
 	LCD_init();
 	KEYPAD_init();
 	
-	uint8_t first_digit, second_digit, opertor;
-	
-	int8_t pressed_key;
-	
+	uint8_t first_arr[max_digits], second_arr[max_digits];
+	uint8_t size1 = 0, size2 = 0, operator = 0;
+	uint32_t first_num = 0, second_num =0;
+		
     while (1) 
     {
 		
 		int8_t key;
 		
-		key = get_input_from_user();
-		if((key >= '0') && (key <= '9')){
-			first_digit = key - '0';
+		for(int i = 0; i < max_digits; i++){
+			key = get_input_from_user();
+			if((key >= '0') && (key <= '9')){
+				first_arr[i]= key - '0';
+				size1++;
+			}
+			else{
+				break;
+			}
 		}
 		
-		key = get_input_from_user();
-		if((key == '+') || (key == '-') || (key == '*') || (key == '/')){
-			opertor = key;
-		}
-		
-		key = get_input_from_user();
-		if((key >= '0') && (key <= '9')){
-			second_digit = key - '0';
+		LCD_write_command(1);
+		// calculate the first number
+		for(int i = 0 ; i < size1; i++){
+			first_num += first_arr[size1-1-i] * pow(10, i);
+			LCD_write_number(first_num);
+			LCD_write_string("->");
+
 		}
 
 		
-		key = get_input_from_user();
-		if(key == '='){
-			LCD_write_number(get_calc_value(first_digit,opertor,second_digit));
+		if(size1 == max_digits){
+			key = get_input_from_user();
 		}
+		
+		// Operator
+		if((key == '+') || (key == '-') || (key == '*') || (key == '/')){
+			operator = key;
+		}
+		
+		for(int i = 0; i < max_digits; i++){
+			key = get_input_from_user();
+			if((key >= '0') && (key <= '9')){
+				second_arr[i]= key - '0';
+				size2++;
+			}
+			else{
+				break;
+			}
+		}
+		
+		// calculate the second number
+		for(int i = 0 ; i < size2; i++){
+			second_num += second_arr[size2-1-i] * pow(10, i) ;
+		}
+		
+		
+		if(size2 == max_digits){
+			key = get_input_from_user();
+		}
+
+		if(key == '='){
+			LCD_write_number(get_calc_value(first_num,operator,second_num));
+		}
+		
+		// reset variables
+		size1 = 0;
+		first_num = 0;
+		size2 = 0;
+		second_num = 0;
 		
 		while(KEYPAD_read() != 'c');
 		LCD_write_command(1);
@@ -67,12 +114,13 @@ uint8_t get_input_from_user(){
 	return key;
 }
 
-int8_t get_calc_value(uint8_t a, uint8_t c, uint8_t b){
+int32_t get_calc_value(uint32_t a, uint8_t c, uint32_t b){
 	switch(c){
 		case '+': return a+b;
 		case '-': return a-b;
 		case '/': return a/b;
 		case '*': return a*b;
 	}
+	return -1;
 }
 
---
+++ Calculator_app/README.md
@@ -0,0 +1,11 @@
+# Calculator APP
+In this project I was capturing the pulse of the PWM signal (T_on) using the ICU.
+
+### Components/drivers Used  in this repo
+* Atmega32
+* LCD
+* PWM
+* ICU
+
+## Demo
+<p align="center"><img src="gifs/ICU_Period.gif"\></p>
